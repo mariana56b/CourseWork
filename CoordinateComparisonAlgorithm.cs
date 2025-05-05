ce80a5a3f1a4990857f3751e91ed11ec014f0aca
mariana56b <marjnabilecka@gmail.com>
2025-05-05 21:02:27 +0300

    New_2

--- CoordinateComparisonAlgorithm.cs
+++ CoordinateComparisonAlgorithm.cs
@@ -12,6 +12,9 @@ namespace CourseWork
         {
             List<Point> polygon = new List<Point>();
             double angleIncrement = 2 * Math.PI / sides;
+
+            var foundPoints = new HashSet<Point>(); // Щоб уникнути повторів
+
             for (int i = 0; i < sides; i++)
             {
                 double angle = i * angleIncrement;
@@ -19,17 +22,20 @@ namespace CourseWork
                 double y = circle.Center.Y + circle.Radius * Math.Sin(angle);
                 Point theoreticalPoint = new Point(x, y);
 
+                // Додаємо більше точності для перевірки відстані
                 foreach (var point in points)
                 {
-                    if (Math.Sqrt(Math.Pow(point.X - theoreticalPoint.X, 2) + Math.Pow(point.Y - theoreticalPoint.Y, 2)) < 0.5)
+                    double distance = Math.Sqrt(Math.Pow(point.X - theoreticalPoint.X, 2) + Math.Pow(point.Y - theoreticalPoint.Y, 2));
+                    if (distance < 0.5 && !foundPoints.Contains(point)) 
                     {
                         polygon.Add(point);
-                        break;
+                        foundPoints.Add(point);
+                        break; 
                     }
                 }
             }
+
             return polygon.Count == sides ? polygon : new List<Point>();
         }
     }
-
 }
